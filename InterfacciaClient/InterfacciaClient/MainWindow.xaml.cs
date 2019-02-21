d6d474bf4319c7cc4816f13edf0eb29b09e5090f
matteoiaconis <matteo.iaconis@virgilio.it>
2019-02-21 09:12:05 +0100

    Implementazione interfaccia (non completa per ora)

---
+++ InterfacciaClient/InterfacciaClient/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.IO;
+
+namespace InterfacciaClient
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            string url = txtURL.Text;
+            GetRequest(url);
+        }
+    }
+    async static void GetRequest(string url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            {
+                using (HttpContent content = response.Content)
+                {//possiamo usare HttpContentHeader headers = content.Headers;
+                    string mycontent = await content.ReadAsStringAsync();
+                    MessageBox.Show(mycontent);
+                }
+
+            }
+
+        }
+    }
